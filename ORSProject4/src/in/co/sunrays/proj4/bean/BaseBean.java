4fa44925dedd11a58bc517a5640abf9ca46e88b3
SunilOS <sunilos007@gmail.com>
2024-09-16 11:10:28 +0530

    Comment added

--- ORSProject4/src/in/co/sunrays/proj4/bean/BaseBean.java
+++ ORSProject4/src/in/co/sunrays/proj4/bean/BaseBean.java
@@ -4,86 +4,145 @@ import java.io.Serializable;
 import java.sql.Timestamp;
 
 /**
- * Parent class of all Beans in application. It contains generic attributes.
+ * Parent class of all Beans in the application. It contains generic attributes
+ * that are common across multiple Beans.
  * 
  * @author SUNRAYS Technologies
  * @version 1.0
  * @Copyright (c) SUNRAYS Technologies
- * 
  */
+public abstract class BaseBean implements Serializable, DropdownListBean, Comparable<BaseBean> {
 
-public abstract class BaseBean implements Serializable, DropdownListBean,
-		Comparable<BaseBean> {
+    /**
+     * Non-business primary key (unique identifier for each record)
+     */
+    protected long id;
 
-	/**
-	 * Non Business primary key
-	 */
-	protected long id;
+    /**
+     * User ID of the person who created this database record.
+     */
+    protected String createdBy;
 
-	/**
-	 * Contains USER ID who created this database record
-	 */
-	protected String createdBy;
+    /**
+     * User ID of the person who last modified this database record.
+     */
+    protected String modifiedBy;
 
-	/**
-	 * Contains USER ID who modified this database record
-	 */
-	protected String modifiedBy;
+    /**
+     * Timestamp indicating when this record was created.
+     */
+    protected Timestamp createdDatetime;
 
-	/**
-	 * Contains Created Timestamp of database record
-	 */
-	protected Timestamp createdDatetime;
+    /**
+     * Timestamp indicating when this record was last modified.
+     */
+    protected Timestamp modifiedDatetime;
 
-	/**
-	 * Contains Modified Timestamp of database record
-	 */
-	protected Timestamp modifiedDatetime;
+    /**
+     * Getter for the ID.
+     * 
+     * @return long - the ID of the record
+     */
+    public long getId() {
+        return id;
+    }
 
-	/**
-	 * accessor
-	 */
-	public long getId() {
-		return id;
-	}
+    /**
+     * Setter for the ID.
+     * 
+     * @param id - the unique identifier to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    /**
+     * Getter for the createdBy field.
+     * 
+     * @return String - the ID of the user who created the record
+     */
+    public String getCreatedBy() {
+        return createdBy;
+    }
 
-	public String getCreatedBy() {
-		return createdBy;
-	}
+    /**
+     * Setter for the createdBy field.
+     * 
+     * @param createdBy - the ID of the user who created the record
+     */
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
+    /**
+     * Getter for the modifiedBy field.
+     * 
+     * @return String - the ID of the user who last modified the record
+     */
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
 
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
+    /**
+     * Setter for the modifiedBy field.
+     * 
+     * @param modifiedBy - the ID of the user who last modified the record
+     */
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
+    /**
+     * Getter for the createdDatetime field.
+     * 
+     * @return Timestamp - the time when the record was created
+     */
+    public Timestamp getCreatedDatetime() {
+        return createdDatetime;
+    }
 
-	public Timestamp getCreatedDatetime() {
-		return createdDatetime;
-	}
+    /**
+     * Setter for the createdDatetime field.
+     * 
+     * @param createdDatetime - the creation timestamp to set
+     */
+    public void setCreatedDatetime(Timestamp createdDatetime) {
+        this.createdDatetime = createdDatetime;
+    }
 
-	public void setCreatedDatetime(Timestamp createdDatetime) {
-		this.createdDatetime = createdDatetime;
-	}
+    /**
+     * Getter for the modifiedDatetime field.
+     * 
+     * @return Timestamp - the time when the record was last modified
+     */
+    public Timestamp getModifiedDatetime() {
+        return modifiedDatetime;
+    }
 
-	public Timestamp getModifiedDatetime() {
-		return modifiedDatetime;
-	}
+    /**
+     * Setter for the modifiedDatetime field.
+     * 
+     * @param modifiedDatetime - the modification timestamp to set
+     */
+    public void setModifiedDatetime(Timestamp modifiedDatetime) {
+        this.modifiedDatetime = modifiedDatetime;
+    }
 
-	public void setModifiedDatetime(Timestamp modifiedDatetime) {
-		this.modifiedDatetime = modifiedDatetime;
-	}
-	
-	public int compareTo(BaseBean next) {
-		return getValue().compareTo(next.getValue());
-	}
+    /**
+     * Compares this bean to another bean based on the value field.
+     * 
+     * @param next - the next BaseBean to compare against
+     * @return int - the result of the comparison
+     */
+    public int compareTo(BaseBean next) {
+        return getValue().compareTo(next.getValue());
+    }
+
+    /**
+     * Abstract method to get the value of the bean, which must be implemented by 
+     * subclasses.
+     * 
+     * @return String - the value to be compared
+     */
+    public abstract String getValue();
 }
